fb948f19301c7ded835267681abd7b396b8677aa
OblivionBun <54904156+OblivionBun@users.noreply.github.com>
2020-08-15 15:25:20 +0300

    α 0.0/200815
    Update build.gradle, try to use Room.

---
+++ app/src/main/java/com/example/freefightjudge/data/User.java
@@ -0,0 +1,12 @@
+package com.example.freefightjudge.data;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey
+    public int id;
+    public String first_name;
+}
