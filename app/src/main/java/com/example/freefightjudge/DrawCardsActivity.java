9b7e64145b3da16c60bcdbc4d027ca618dbce58a
OblivionBun <54904156+OblivionBun@users.noreply.github.com>
2020-09-25 17:56:27 +0300

    α 0.0.0.18/200925
    Update draw cards activity

--- app/src/main/java/com/example/freefightjudge/DrawCardsActivity.java
+++ app/src/main/java/com/example/freefightjudge/DrawCardsActivity.java
@@ -2,12 +2,16 @@ package com.example.freefightjudge;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class DrawCardsActivity extends AppCompatActivity {
+  private Toolbar toolbar;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_draw_cards);
+
+    toolbar = findViewById(R.id.draw_cards_tbar);
   }
 }
