a13c4c74db948bb702645d40517681354464b641
OblivionBun <54904156+OblivionBun@users.noreply.github.com>
2020-09-30 23:23:15 +0300

    α 0.0.0.21/200930
    Update fight activity

--- app/src/main/java/com/example/freefightjudge/FightActivity.java
+++ app/src/main/java/com/example/freefightjudge/FightActivity.java
@@ -2,12 +2,17 @@ package com.example.freefightjudge;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class FightActivity extends AppCompatActivity {
+  private Toolbar toolbar;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_fight);
+
+    toolbar = (Toolbar) findViewById(R.id.fight_tbar);
+    toolbar.setTitle(R.string.txt_fight);
   }
 }
