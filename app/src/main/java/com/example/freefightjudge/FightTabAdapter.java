13d9e784c0e09f9be75561f56b76d68d716bb024
OblivionBun <54904156+OblivionBun@users.noreply.github.com>
2020-10-05 23:53:11 +0300

    α 0.0.0.24/201005
    Update drawable and activity_fight.xml

---
+++ app/src/main/java/com/example/freefightjudge/FightTabAdapter.java
@@ -0,0 +1,28 @@
+package com.example.freefightjudge;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FightTabAdapter extends FragmentPagerAdapter {
+  public FightTabAdapter(@NonNull FragmentManager fm) {
+    super(fm);
+  }
+
+  public FightTabAdapter(@NonNull FragmentManager fm, int behavior) {
+    super(fm, behavior);
+  }
+
+  @NonNull
+  @Override
+  public Fragment getItem(int position) {
+    return null;
+  }
+
+  @Override
+  public int getCount() {
+    return 0;
+  }
+}
+}
